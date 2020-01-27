e91af850bfea33a13ae5f013139e92e71a29879f
guerra <rua.brahiam>
2020-01-27 21:21:01 +0100

    exo1

---
+++ src/JS/main.js
@@ -0,0 +1,14 @@
+let ajouter= document.getElementById("exo1-txt")
+ajouter.innerHTML= "Mon texte remodifiée";
+
+
+let selection= document.querySelector('h1')
+
+console.log(selection.classList[0]);
+selection.classList.remove("bg-success")
+selection.classList.add("bg-warning", "text-dark")
+console.log(selection.classList)
+
+
+
+
